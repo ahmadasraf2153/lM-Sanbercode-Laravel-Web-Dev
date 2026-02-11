d1c4255a45616857b7412d84d2afe9c46af7f041
AHMAD ASHRAF <ahmadasraf2153@gmail.com>
2026-02-11 20:39:02 +0700

    menambahkan tugas 14 sanbercode

--- inventoryapp_asraf/database/migrations/2026_02_11_133301_create_profiles_table.php
+++ inventoryapp_asraf/database/migrations/2026_02_11_133301_create_profiles_table.php
@@ -8,18 +8,21 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->enum('role', ['staff','admin'])->default('staff');
+            $table->integer('age');
+            $table->text('bio');
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profiles');
     }
 };
